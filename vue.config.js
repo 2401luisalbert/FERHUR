7958bc57d28797f4d5f747a4dd11c91cc3188eeb
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2024-06-13 21:40:41 -0600

    change base URL vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: 'https//2401luisalbert.github.io/FERHUR/ferhur'
+  base: 'https//2401luisalbert.github.io/FERHUR/'
 })
