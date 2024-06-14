ddafd3af5760843910d8d74e50dc7a3d4052b255
2401luisalbert <125489391+2401luisalbert@users.noreply.github.com>
2024-06-13 21:43:23 -0600

    Change base to publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: 'https//2401luisalbert.github.io/FERHUR/'
+  publicPath: 'https://2401luisalbert.github.io/FERHUR/'
 })
